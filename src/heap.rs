5637ebe302639287c0618a022af4d85a1aa91c1c
Chris Fallin <chris@cfallin.org>
2022-09-23 21:43:44 -0700

    WIP.

--- src/heap.rs
+++ src/heap.rs
@@ -1,7 +1,7 @@
 //! Static heap image summary.
 
 use std::collections::HashMap;
-use walrus::{MemoryId, Module};
+use walrus::{ActiveData, ActiveDataLocation, DataKind, Memory, MemoryId, Module};
 
 #[derive(Clone, Debug)]
 pub struct Summaries {
@@ -16,6 +16,38 @@ pub struct Summary {
 
 pub fn build_summaries(module: &Module) -> anyhow::Result<Summaries> {
     Ok(Summaries {
-        heaps: HashMap::default(),
+        heaps: module
+            .memories
+            .iter()
+            .flat_map(|mem| maybe_summarize(module, mem).map(|summary| (mem.id(), summary)))
+            .collect(),
     })
+}
+
+fn maybe_summarize(module: &Module, mem: &Memory) -> Option<Summary> {
+    const WASM_PAGE: usize = 1 << 16;
+    let len = (mem.initial as usize) * WASM_PAGE;
+    let mut image = vec![0; len];
+
+    for &segment_id in &mem.data_segments {
+        let segment = module.data.get(segment_id);
+        match segment.kind {
+            DataKind::Passive => continue,
+            DataKind::Active(ActiveData {
+                memory,
+                location: ActiveDataLocation::Relative(..),
+            }) => {
+                return None;
+            }
+            DataKind::Active(ActiveData {
+                memory,
+                location: ActiveDataLocation::Absolute(offset),
+            }) => {
+                let offset = offset as usize;
+                image[offset..(offset + segment.value.len())].copy_from_slice(&segment.value[..]);
+            }
+        }
+    }
+
+    Some(Summary { image, len })
 }
